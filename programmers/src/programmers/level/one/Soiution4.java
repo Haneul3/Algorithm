ad4e07cbf6a027d46c38eaa46ef65a3a243a96dd
Ha_NeuL <khn9703@gmail.com>
2023-02-13 21:24:17 +0900

    2023-02-13

---
+++ programmers/src/programmers/level/one/Soiution4.java
@@ -0,0 +1,16 @@
+package programmers.level.one;
+
+public class Soiution4 {
+	boolean solution(String s) {
+		char[] arr = s.toCharArray();
+		int cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 'p' || arr[i] == 'P')
+				cnt++;
+			else if (arr[i] == 'y' || arr[i] == 'Y')
+				cnt--;
+		}
+		return cnt == 0;
+	}
+
+}
