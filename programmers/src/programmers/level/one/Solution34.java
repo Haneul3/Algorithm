23395e98c2b1128a53f36074799fcf08a7617987
Ha_NeuL <khn9703@gmail.com>
2023-04-01 23:05:25 +0900

    2023-04-01 아이스 아메리카노, 배열자르기

--- programmers/src/programmers/level/one/Solution34.java
+++ programmers/src/programmers/level/one/Solution34.java
@@ -1,13 +1,24 @@
 package programmers.level.one;
 
+import java.util.Arrays;
+
 public class Solution34 {
-	public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        for(int i=0; i<num_list.length; i++){
-            answer[num_list.length-i-1] = num_list[i];
-        }
+	// 아이스 아메리카노
+	public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money/5500;
+        answer[1] = money%5500;
         return answer;
     }
 	
-	// Collections.reverse 함수
+	public int[] solution2(int money) {
+        return new int[] { money / 5500, money % 5500 };
+    }
+	
+	// 배열자르기 Arrays.copyOfRange 이해 필요
+	class Solution {
+	    public int[] solution(int[] numbers, int num1, int num2) {
+	        return Arrays.copyOfRange(numbers, num1, (num2 + 1));
+	    }
+	}
 }
