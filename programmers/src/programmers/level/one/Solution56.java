e86114d2595bebed719e3e89634d5badf905ba87
Ha_NeuL <khn9703@gmail.com>
2023-04-26 22:43:25 +0900

    2023-04-26 짝수는 싫어요

---
+++ programmers/src/programmers/level/one/Solution56.java
@@ -0,0 +1,23 @@
+package programmers.level.one;
+
+public class Solution56 {
+	// 짝수는 싫어요
+	public int[] solution(int n) {
+		int[] answer = {};
+
+		if (n % 2 == 1)
+			answer = new int[n / 2 + 1];
+		else
+			answer = new int[n / 2];
+
+		int cnt = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 1) {
+				answer[cnt] = i;
+				cnt++;
+			}
+		}
+
+		return answer;
+	}
+}
